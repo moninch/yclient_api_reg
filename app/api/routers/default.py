acbda962d859407aca3e520898e183174efefdad
Даниил <mon.inch@yandex.ru>
2024-11-25 19:02:10 +0300

    add function to add record.

--- app/api/routers/default.py
+++ app/api/routers/default.py
@@ -54,3 +54,29 @@ async def get_book_times(
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
             return {"status": response.status, "response": await response.json()}
+
+
+@router.post("/book_record")
+async def book_record(company_id: int = 1177126):  # ID of the company
+    url = f"https://api.yclients.com/api/v1/book_record/{company_id}"
+    headers = {
+        "Authorization": f"Bearer {SETTINGS.PARTNER_TOKEN}",
+        "Content-Type": "application/json",
+        "Accept": "application/vnd.yclients.v2+json",
+    }
+    data = {
+        "phone": "79000000000",
+        "fullname": "John Doe",
+        "email": "sosa@mail.ru",
+        "appointments": [
+            {
+                "id": 176122723433,
+                "staff_id": 0,
+                "services": [17612273],
+                "datetime": "2024-11-25T19:00:00+03:00",
+            }
+        ],
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=data, headers=headers) as response:
+            return {"status": response.status, "response": await response.json()}
