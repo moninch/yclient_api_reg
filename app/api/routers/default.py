4ddfdc68ea22511efe904d8aec6ce475b54c2f6b
Даниил <mon.inch@yandex.ru>
2024-11-27 13:54:07 +0300

    Delete useless url

--- app/api/routers/default.py
+++ app/api/routers/default.py
@@ -4,7 +4,6 @@ import aiohttp
 from starlette.responses import JSONResponse
 from app.settings import SETTINGS
 
-# https://n1290414.yclients.com/company/1177126/personal/select-time?o=m3570915s17612273d2425111900
 router = APIRouter()
 
 
