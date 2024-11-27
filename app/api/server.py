04bc87f064dc371946d8ee0c59dee1f49d2a0a90
Даниил <mon.inch@yandex.ru>
2024-11-27 12:37:37 +0300

    Add routers in server.py

--- app/api/server.py
+++ app/api/server.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from app.api.routers.default import router as default_router
+from app.api.routers.companies import router as companies_router
+from app.api.routers.bookings import router as bookings_router
+from app.api.routers.integration import router as integration_router
 from app.settings import SETTINGS
 from app.utils.mongodb import MongoDB
 
 app = FastAPI()
 
-app.include_router(default_router)
+app.include_router(default_router, prefix="/api/v1", tags=["Default"])
+app.include_router(companies_router, prefix="/api/v1/companies", tags=["Companies"])
+app.include_router(bookings_router, prefix="/api/v1/bookings", tags=["Bookings"])
+app.include_router(
+    integration_router, prefix="/api/v1/integration", tags=["Integration"]
+)
 
 
 @app.on_event("startup")
