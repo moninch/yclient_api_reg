b74c9d3520690e28fc3f1f34cd86fbf7e32d60c9
Даниил <mon.inch@yandex.ru>
2024-11-27 14:03:14 +0300

    Added the ability to find a branch in the database by url

--- app/api/routers/integration.py
+++ app/api/routers/integration.py
@@ -1,22 +1,28 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
+from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
+from app.__main__ import MongoDB
 
 router = APIRouter()
 
 
+# https://n1290414.yclients.com/company/1177126/personal/select-time?o=m3570915s17612273d2425111900
 @router.get("/check_integration")
-async def check_integration(url: str):
+async def check_integration(url: str = Query(..., description="Ссылка клиента")):
     """
-    Check if a company has installed the integration
-    :param url: Integration link
-    :return: JSON response with the company status
+    Проверить наличие компании в базе по предоставленному URL.
     """
-    branch_id = url.split("/company/")[1].split("/")[0]
-    # Проверить наличие branch_id в вашей базе данных
-    # Пример: поиск в MongoDB
-    # company = companies_collection.find_one({"id": int(branch_id)})
-    company = None  # Замените на реальную логику
+    try:
+        # Извлекаем branch_id из URL
+        branch_id = int(url.split("/company/")[1].split("/")[0])
+    except (IndexError, ValueError):
+        raise HTTPException(status_code=400, detail="Неверный формат URL")
+
+    company = await MongoDB.db.get_collection("companies").find_one(
+        {"branch_id": branch_id}
+    )
     if company:
-        return JSONResponse(content={"status": "integrated", "company": company})
+        company["_id"] = str(company["_id"])
+        return jsonable_encoder({"status": "integrated", "company": company})
     else:
-        raise HTTPException(status_code=404, detail="Company not found.")
+        raise HTTPException(status_code=404, detail="Компания не найдена")
