d3707dd28fd43b90021e07bcac785819dfa49c00
Даниил <mon.inch@yandex.ru>
2024-11-27 13:53:39 +0300

    Companies add in mongoDB like this _id 6746f996c06aa67607d195c4 branch_id 1177126 city "Таганрог" email "" title "Bebriys"

--- app/api/routers/companies.py
+++ app/api/routers/companies.py
@@ -1,8 +1,9 @@
 import aiohttp
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, requests
+from pymongo import MongoClient
 
 from app.settings import SETTINGS
-
+from app.__main__ import MongoDB
 
 router = APIRouter()
 
@@ -10,9 +11,7 @@ router = APIRouter()
 @router.get("/companies_info")
 async def get_my_companies_info():
     """
-    Get the list of companies that have installed the integration
-
-    :return: JSON response with the list of companies
+    Обновить список компаний, установивших интеграцию.
     """
     url = "https://api.yclients.com/api/v1/companies"
     headers = {
@@ -21,7 +20,24 @@ async def get_my_companies_info():
         "Accept": "application/vnd.yclients.v2+json",
     }
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            url, params={"my": 1}, headers=headers
-        ) as response:  # используем параметр my=1, чтобы смотреть только компании, которые установили интеграцию
-            return {"status": response.status, "response": await response.json()}
+        async with session.get(url, params={"my": 1}, headers=headers) as response:
+            if response.status != 200:
+                raise HTTPException(
+                    status_code=response.status, detail="Ошибка при получении компаний"
+                )
+
+            companies = (await response.json()).get("data", [])
+            for company in companies:
+                company_data = {
+                    "branch_id": company["id"],
+                    "title": company["title"],
+                    "email": company.get("email", ""),
+                    "city": company.get("city", ""),
+                }
+                MongoDB.db.get_collection("companies").update_one(
+                    {"branch_id": company_data["branch_id"]},
+                    {"$set": company_data},
+                    upsert=True,
+                )
+
+            return {"message": "Компании обновлены", "count": len(companies)}
